8982605c5f3681d35762424b72165a992f0b0472
JacobMabey <mabey.jacob@gmail.com>
2024-02-27 12:47:41 -0700

    Began working on fetching api calls

--- lib/notes.dart
+++ lib/notes.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:everthought/edit_note.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class NotesPage extends StatefulWidget {
   const NotesPage({super.key});
@@ -17,11 +20,51 @@ class NotesState extends State<NotesPage> {
     });
   }
 
+  Future<String> fetchApiData(String path) async {
+    print('hello');
+    var response = await http.get(Uri.https('swapi.dev', "/api/$path"));
+    return response.body;
+  }
+
+  dynamic getApiPerson(int id) async {
+    dynamic jsonData;
+    await fetchApiData('people/$id').then((value) => {
+      jsonData = jsonDecode(value) as Map<String, dynamic>
+    });
+    return jsonData;
+  }
+
+  Future<String> getApiName(int id) async {
+    dynamic data = await getApiPerson(id);
+    return data['name'];
+  }
+
   @override
   Widget build(BuildContext context) {
-    
+    final data = getApiName(1);
+
     return Scaffold(
-      body: const Text('Welcome to Notes!'),
+      //body: Text(data["name"]),
+      body: FutureBuilder(
+        future: data,
+        initialData: "Code sample",
+        builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          } else {
+            final data = snapshot.data as String;
+            return Text(data);
+          }
+        }
+        else {
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Colors.blue,
+            ),
+          );
+        }
+      }),
 
       floatingActionButton: FloatingActionButton(
         onPressed: _addNote,
